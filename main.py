b4084042c2e2453bbf3fb2fccb0d58bf333b757f
221RDB178 <117149941+221RDB187@users.noreply.github.com>
2023-03-21 17:19:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ def parallel_processing(n, m, data):
 
     for j in range(m):
         thread = 0
-        for i in range(n, 0):
+        for i in range(0, n):
             if threads[i][1]< threads[thread][1]:
-                thread = 1
+                thread = i
         index, times = threads[thread]
         output.append(threads[thread])
         threads[thread] = (index, times + data[j])
@@ -23,23 +23,20 @@ def main():
     # first line - n and m
     # n - thread count 
     # m - job count
-   
+    # second line - data 
+    # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
+    # TODO: create the function
 
     first = input().split()
     n = int(first[0])
     m = int(first[1])
     data = list(map(int, input().split()))
-
-    # second line - data 
-    # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
-
-    # TODO: create the function
     result = parallel_processing(n,m,data)
+    for r in result:
+        print(r[0], r[1])
     
     # TODO: print out the results, each pair in it's own line
 
-    for r in result:
-        print(r[0], r[1])
 
 if __name__ == "__main__":
     main()
